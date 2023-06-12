37ea223ae7875481f7bc7e880661824b03f8f8fa
Stephen Gaito <stephen@perceptisys.co.uk>
2023-06-12 13:26:53 +0100

    updated scopeActions to include an explicit trace variable

--- src/actions/contextActions.mjs
+++ src/actions/contextActions.mjs
@@ -1,21 +1,25 @@
 
 export function registerActions(ScopeActions) {
 
-  function loadComponent(thisScope, theScope, theTokens, callArgs) {
-    console.log("----------------------------------------------------------")
-    console.log("loadComponent")
-    console.log(`thisScope: ${thisScope}`)
-    console.log(` theScope: ${theScope}`)
-    console.log(`theTokens: ${theTokens}`)
-    console.log(` callArgs: ${callArgs}`)
-    console.log("----------------------------------------------------------")
-  }
-
-  ScopeActions.addScopedAction(
-    'keyword.control.structure.context',
-    import.meta.url,
-    { },
-    loadComponent
+  //function loadComponent(thisScope, theScope, theTokens, callArgs) {
+    
+    
+    ScopeActions.addScopedAction(
+      'keyword.control.structure.context',
+      import.meta.url,
+      { },
+      async function(thisScope, theScope, theTokens, theLine, theDoc, trace) {
+        if (trace) {
+          console.log("----------------------------------------------------------")
+          console.log("loadComponent")
+          console.log(`thisScope: ${thisScope}`)
+          console.log(` theScope: ${theScope}`)
+          console.log(`theTokens: ${theTokens}`)
+          console.log(`  theLine: ${theLine}`)
+          console.log(`   theDoc: ${theDoc}`)
+          console.log("----------------------------------------------------------")
+        }
+      }
   )
 
 }
